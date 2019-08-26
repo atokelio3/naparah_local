73a4bceb55772ca48f99b85af613fcafcdfb6cd1
Boris Safonov <bsafonov@webim.ru>
2019-08-26 11:18:19 +0300

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 // const browserSync = require('browser-sync').create();
 
 function styles() {
   return gulp.src('./gulp/**/*.scss')
-   .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./static/css'));
-    // .pipe(browserSync.stream());
+      .pipe(sass().on('error', sass.logError))
+      .pipe(autoprefixer({
+        overrideBrowserslist: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(gulp.dest('./static/css'));
+  // .pipe(browserSync.stream());
 }
 
 function watch() {
